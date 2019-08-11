e64fbe54f60b94a7adf04969fc35924b633bd7ad
root <root@ip-172-31-80-10.ec2.internal>
2019-08-11 12:23:47 +0000

    My first commit

---
+++ shell-scripting/loopsample2.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+echo " enter your name "
+read " user name "
+echo " enter your password "
+read " password "
+if [ $username == /root]
+echo "valid user "
+else 
+echo " invalid user "
+fi 
---
+++ shell-scripting/loopsamples.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+a=10
+if [ $a -lt 10 ];
+then
+echo " it is a single digit number "
+else 
+echo " it is a double digit number"
+fi
+
